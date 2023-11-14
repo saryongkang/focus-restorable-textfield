358bea67cfa95b275d5d86f72b02b1b3275cb225
Sa-ryong Kang <saryongkang@google.com>
2023-11-13 19:56:33 -0800

    Add FocusRestorer

--- app/src/main/java/com/example/focusrestorabletextfield/HomeScreen.kt
+++ app/src/main/java/com/example/focusrestorabletextfield/HomeScreen.kt
@@ -20,8 +20,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.focusrestorabletextfield.ui.theme.FocusRestorableTextfieldTheme
@@ -39,6 +42,7 @@ fun HomeScreen(modifier: Modifier = Modifier) {
             Spacer(modifier = Modifier.height(16.dp))
             TextFieldPanel()
             Spacer(modifier = Modifier.height(32.dp))
+            FocusRestorableTextFieldPanel()
             Spacer(Modifier.windowInsetsBottomHeight(WindowInsets.ime))
         }
 
@@ -75,6 +79,58 @@ fun TextFieldPanel(modifier: Modifier = Modifier) {
             value = text3,
             onValueChange = setText3,
             modifier = Modifier.widthIn(max = 240.dp).fillMaxWidth(),
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
+
+@Composable
+fun FocusRestorableTextFieldPanel(modifier: Modifier = Modifier) {
+    val (text1, setText1) = rememberSaveable(stateSaver = TextFieldValue.Saver) {
+        mutableStateOf(TextFieldValue(""))
+    }
+    val (text2, setText2) = rememberSaveable(stateSaver = TextFieldValue.Saver) {
+        mutableStateOf(TextFieldValue(""))
+    }
+    val (text3, setText3) = rememberSaveable(stateSaver = TextFieldValue.Saver) {
+        mutableStateOf(TextFieldValue(""))
+    }
+    val focusRequester1 = remember { FocusRequester() }
+    val focusRequester2 = remember { FocusRequester() }
+    val focusRequester3 = remember { FocusRequester() }
+    Column(
+        modifier = modifier
+            .padding(horizontal = 16.dp)
+            .fillMaxWidth()
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Focus Restorable TextFields")
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = text1,
+            onValueChange = setText1,
+            modifier = Modifier
+                .widthIn(max = 240.dp)
+                .fillMaxWidth()
+                .focusRestorer(focusRequester1),
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = text2,
+            onValueChange = setText2,
+            modifier = Modifier
+                .widthIn(max = 240.dp)
+                .fillMaxWidth()
+                .focusRestorer(focusRequester2, false),
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = text3,
+            onValueChange = setText3,
+            modifier = Modifier
+                .widthIn(max = 240.dp)
+                .fillMaxWidth()
+                .focusRestorer(focusRequester3, false),
         )
         Spacer(modifier = Modifier.height(16.dp))
     }
